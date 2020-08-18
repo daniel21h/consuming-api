38de88e30aa629cb2d0ca52296402e3d93cc5762
daniel21h <hesseldaniel21@gmail.com>
2020-08-18 07:58:39 -0300

    feat: connecting two api's using axios

--- src/server.js
+++ src/server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
-const server = express()
+const server = express();
 
-server.use(cors())
+server.use(cors());
 
-server.get('/', (request, response) => {
+server.get('/', async (request, response) => {
   return response.json([
     { name: 'Daniel' },
     { name: 'Rafael' },
-  ])
-})
+  ]);
+});
+
+server.get('/jsonplaceholder', async (request, response) => {
+  const { data } = await axios ('https://jsonplaceholder.typicode.com/users');
+  
+  return response.json(data);
+});
 
 server.listen(3333, () => {
   console.log('Server running on port 3333.')
-})
+});
